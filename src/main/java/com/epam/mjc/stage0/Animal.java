6e4de61f4855a5d4074fcc2c863bb1b66a842df9
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2022-08-29 01:01:00 +0500

    Update Animal.java

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -11,6 +11,7 @@ public class Animal {
         this.hasFur = hasFur;
     }
     public String getDescription(){
-        return "This animal is mostly " + color + ". It has " + numberOfPaws + (numberOfPaws == 1 ? " pow":" pows") + " and " + (hasFur ? "a":"no") + " fur.";
+        String result="This animal is mostly " + color + ". It has " +numberOfPaws+ (numberOfPaws == 1 ? " paw" : " paws") + " and " + (hasFur ? "a" : "no") + " fur.";
+        return result;
     }
 }
